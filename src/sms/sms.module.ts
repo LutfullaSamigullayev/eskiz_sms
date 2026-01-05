0304c5c2fbc99d6b3b62452947da59d468c14c18
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-05 15:10:05 +0500

    export SmsService

--- src/sms/sms.module.ts
+++ src/sms/sms.module.ts
@@ -2,8 +2,7 @@ import { Module } from "@nestjs/common";
 import { SmsService } from "./sms.service";
 
 @Module({
-  imports: [],
-  controllers: [SmsService],
   providers: [SmsService],
+  exports: [SmsService],
 })
 export class SmsModule {}
