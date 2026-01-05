5c593598ed6627878e3e52628328c264b2e763b5
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-05 19:23:23 +0500

    added set and get redisService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from "@nestjs/common";
+import { RedisService } from "./redis/redis.service";
 
 @Controller()
 export class AppController {
+  constructor(private redisService: RedisService) {}
 
+  @Get("get")
+  async getHello() {
+    let result = await this.redisService.get("mykey");
+    return result;
+  }
 
+  @Post("set")
+  async post() {
+    await this.redisService.set("mykey", "myvalue");
+    return "OK";
+  }
 }
