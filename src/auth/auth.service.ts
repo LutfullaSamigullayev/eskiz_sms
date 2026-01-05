119325a778c8b45ffed6b4c54cd50527c1c0d4fa
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-05 15:09:20 +0500

    update auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,27 @@
-import { SmsService } from './../sms/sms.service';
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Auth } from './entities/auth.entity';
-import { Repository } from 'typeorm';
+import { SmsService } from "./../sms/sms.service";
+import { Injectable } from "@nestjs/common";
+import { CreateAuthDto } from "./dto/create-auth.dto";
+import { UpdateAuthDto } from "./dto/update-auth.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Auth } from "./entities/auth.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(Auth) private authRepo: Repository<Auth>, private SmsService: SmsService) {}
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(Auth) private authRepo: Repository<Auth>,
+    private SmsService: SmsService
+  ) {}
+
+  async create(createAuthDto: CreateAuthDto) {
+    const { email, phone_number } = createAuthDto;
+    await this.SmsService.senOtp(phone_number);
+    const user = this.authRepo.create({
+      email,
+      phone_number,
+      otp: "123456",
+    });
+    await this.authRepo.save(user);
+    return "This action adds a new auth";
   }
 }
